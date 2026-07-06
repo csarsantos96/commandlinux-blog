16cda24831b9db77c9584b91124726ec285e0a79
Cesar Santos <csar.santos18@gmail.com>
2026-07-06 15:57:19 -0500

    feat: add favicon and implement blinking effect in the browser tab

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
+import { startBlinkingFavicon } from './lib/blinkFavicon'
+
+startBlinkingFavicon()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
